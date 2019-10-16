7df88307bdfa6a30c377b3dae98f9609845534c5
saja amer <sajaamer86@gmail.com>
2019-10-16 14:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ BoardMember.prototype.releasePressStatement=function(){
   return 'You will see great things from Scuber.'
 } 
 BoardMember.prototype.sayHi=function(){
-  return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+  return 
 } 
